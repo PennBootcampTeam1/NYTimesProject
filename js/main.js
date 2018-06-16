d1aa087bca73eeb1eb4b241cba379eb9f25eb483
amh5366 <amh5366@upenn.edu>
2018-06-16 10:46:56 -0400

    added ajax call

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 console.log('connected');
-var searchInput = $("search");
-var recordCountInput = $("numRecords");
-var startInput = $("startYear");
-var endInput = $("endYear");
-var searchBtn = $("search_button");
-var clearBtn = $("clearResults");
-var ResultsDiv = $("results");
+var searchInput = $("#search");
+var recordCountInput = $("#numRecords");
+var startInput = $("#startYear");
+var endInput = $("#endYear");
+var searchBtn = $("#search_button");
+var clearBtn = $("#clearResults");
+var ResultsDiv = $("#results");
+
+
+function displayArticles(response) {
+    
+    console.log(response);
+}
+    
+
+function makeCall() {
+    var queryURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=apple&api-key=491ffd3e66c94f8ba2eaa87512012d3d'
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(displayArticles);
+    }
+
+
+searchBtn.on("click", makeCall);
+
