fba1662698221a41b18802dca8ef4025f257bed8
Torning, Fraser <Fraser_Torning@comcast.com>
2018-06-16 10:43:58 -0400

    added param vars

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ var endInput = $("endYear");
 var searchBtn = $("search_button");
 var clearBtn = $("clearResults");
 var ResultsDiv = $("results");
+var apiKey = 'd9e270cb22be4bbb948370c965cb462d';
+var queryUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=';
+var paramSort = '?sort=newest';
+var paramHighlight = '?hl=true';
+var paramPagination = '?page=20';
+
+
+function getSearch()
