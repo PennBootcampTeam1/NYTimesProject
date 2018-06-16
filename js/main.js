8671ae0b0c38ffc714f927568d66876332b1da55
Torning, Fraser <Fraser_Torning@comcast.com>
2018-06-16 10:32:41 -0400

    added js vars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log('connected');
+var searchInput = $("search");
+var recordCountInput = $("numRecords");
+var startInput = $("startYear");
+var endInput = $("endYear");
+var searchBtn = $("search_button");
+var clearBtn = $("clearResults");
+var ResultsDiv = $("results");
